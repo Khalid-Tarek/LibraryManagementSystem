ea588fc042849b888b366457f1c4858096f1978e
Kai22 <kid22500@gmail.com>
2020-11-16 17:06:12 +0200

    Fixed a problem in Main.java

--- src/librarymanagementsystem/Main.java
+++ src/librarymanagementsystem/Main.java
@@ -6,9 +6,8 @@ package librarymanagementsystem;
  */
 public class Main {
 
-    public static void main() {
+    public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
-    }
+        }
     
 }
